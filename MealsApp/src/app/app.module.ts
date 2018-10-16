ffe926989024a0bd03badc7c0004f1a2940e1cfc
Bartosz97 <bartoszdudek5@wp.pl>
2018-10-16 21:02:46 +0200

    Use a dropdown directive

--- MealsApp/src/app/app.module.ts
+++ MealsApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RecipeItemComponent } from './recipies/recipe-list/recipe-item/recipe-i
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
+import { DropdownDirective } from './shared/dropdown.directive';
 
 @NgModule({
    declarations: [
@@ -19,7 +20,8 @@ import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.co
       RecipeListComponent,
       RecipeItemComponent,
       ShoppingListComponent,
-      ShoppingEditComponent
+      ShoppingEditComponent,
+      DropdownDirective
    ],
    imports: [
       BrowserModule
