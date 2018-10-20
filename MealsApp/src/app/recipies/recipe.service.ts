57fdf9482b859b9e62563a80c1c15094c1bdf629
Bartosz97 <bartoszdudek5@wp.pl>
2018-10-21 00:44:00 +0200

    Add more functionality with services

--- MealsApp/src/app/recipies/recipe.service.ts
+++ MealsApp/src/app/recipies/recipe.service.ts
@@ -1,14 +1,26 @@
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
 import { Recipe } from './recipe.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
+@Injectable()
 export class RecipeService {
 recipeSelected = new EventEmitter<Recipe>();
 
    private recipes: Recipe[] = [
-        new Recipe('Testowy przepis', 'cos tam', 'http://bi.gazeta.pl/im/a0/b1/10/z17504416V,Marek-Kaminski.jpg')
+        new Recipe('Testowy przepis', 'cos tam', 'http://bi.gazeta.pl/im/a0/b1/10/z17504416V,Marek-Kaminski.jpg',[
+            new Ingredient('Mięso', 1),
+            new Ingredient('Frytki', 20)
+        ])
       ];
+
+      constructor(private shoppingListService: ShoppingListService) {}
 
       getRecipes() {
           return this.recipes.slice();
+      }
+
+      addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.shoppingListService.addIngredients(ingredients);
       }
 }
