e2c95ea7bc196207a108f1e2d441a6e71eb897ca
Bartosz97 <bartoszdudek5@wp.pl>
2018-11-12 13:06:24 +0100

    Create a shopping list feature module and load components via selectors

--- MealsApp/src/app/app-routing.module.ts
+++ MealsApp/src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
-import { AuthGuard } from './auth/auth-guard.service';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { RecipeEditComponent } from './recipies/recipe-edit/recipe-edit.component';
-import { RecipeDetailComponent } from './recipies/recipe-detail/recipe-detail.component';
-import { RecipeStartComponent } from './recipies/recipe-start/recipe-start.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import { RecipiesComponent } from './recipies/recipies.component';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
     {path: 'shopping-list', component: ShoppingListComponent},
-    {path: 'signup', component: SignupComponent},
-    { path: 'signin', component: SigninComponent}
+
 ];
 
 @NgModule({
