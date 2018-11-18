b61227afc3e1a21e952c52953f07814e6e87e924
Bartosz97 <bartoszdudek5@wp.pl>
2018-11-18 13:15:26 +0100

    Add some changes with HttpClient

--- MealsApp/src/app/app.module.ts
+++ MealsApp/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { AuthModule } from './auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -15,8 +15,8 @@ import { CoreModule } from './core/core.module';
    ],
    imports: [
       BrowserModule,
+      HttpClientModule,
       AppRoutingModule,
-      HttpModule,
       SharedModule,
       ShoppingListModule,
       AuthModule,
--- MealsApp/src/app/shared/data-storage.service.ts
+++ MealsApp/src/app/shared/data-storage.service.ts
@@ -1,28 +1,36 @@
 import { Recipe } from './../recipies/recipe.model';
 import { RecipeService } from './../recipies/recipe.service';
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable()
 export class DataStorageService {
-    constructor(private http: Http, private recipeService: RecipeService, private authService: AuthService) {
+    constructor(private httpClient: HttpClient, private recipeService: RecipeService, private authService: AuthService) {
 
     }
 
     storeRecipes() {
         const token = this.authService.getToken();
-        return this.http.put('https://recipebook-e10a2.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
+  //      const headers = new HttpHeaders().set('Authorization', 'Bearer safdfaf');
+
+        return this.httpClient.put('https://recipebook-e10a2.firebaseio.com/recipes.json', this.recipeService.getRecipes(), {
+            observe: 'body',
+            params: new HttpParams().set('auth', token)
+   //         headers: headers
+        });
     }
 
     getRecipes() {
         const token = this.authService.getToken();
 
-        this.http.get('https://recipebook-e10a2.firebaseio.com/recipes.json?auth=' + token)
+        this.httpClient.get<Recipe[]>('https://recipebook-e10a2.firebaseio.com/recipes.json?auth=' + token, {
+            observe: 'body',
+            responseType: 'json'
+        })
         .pipe(map(
-            (response: Response) => {
-                const recipes: Recipe[] = response.json();
+            (recipes) => {
                 for (const recipe of recipes) {
                     if (!recipe['ingredients']) {
                         recipe['ingredients'] = [];
