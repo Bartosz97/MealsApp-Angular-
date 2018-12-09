efa8374ac6ce599a7df51c79e60e7cc4031cea1a
Bartosz97 <bartoszdudek5@wp.pl>
2018-12-09 20:02:36 +0100

    Dispatching update and deleting shopping list actions

--- MealsApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ MealsApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -38,7 +38,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
-      this.shoppingListService.updateIngredient(this.editedItemIndex, newIngredient);
+      this.store.dispatch(new ShoppingListActions.UpdateIngredient({index: this.editedItemIndex, ingredient: newIngredient}));
     } else {
       this.store.dispatch(new ShoppingListActions.AddIngredient(newIngredient));
     }
@@ -52,7 +52,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
   }
 
   onDelete() {
-    this.shoppingListService.deleteIngredient(this.editedItemIndex);
+    this.store.dispatch(new ShoppingListActions.DeleteIngredient(this.editedItemIndex));
     this.onClear();
   }
   ngOnDestroy() {
