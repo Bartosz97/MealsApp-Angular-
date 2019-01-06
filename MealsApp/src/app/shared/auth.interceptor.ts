7ceac4e6b04e4794f14f218053f802d8fdafc041
Bartosz97 <bartoszdudek5@wp.pl>
2019-01-06 14:12:19 +0100

    Handling the Auth Token

--- MealsApp/src/app/shared/auth.interceptor.ts
+++ MealsApp/src/app/shared/auth.interceptor.ts
@@ -1,16 +1,24 @@
+import 'rxjs/add/operator/switchMap';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import { AuthService } from '../auth/auth.service';
+
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
+import { Store } from '@ngrx/store';
+import { take, switchMap } from 'rxjs/operators';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-    constructor(private authService: AuthService) {}
+    constructor(private store: Store<fromApp.AppState>) {}
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         console.log('Intercepted!', req);
         // const copiedReq = req.clone({headers: req.headers.append('', '')});
-        const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
-        return next.handle(copiedReq);
+        return this.store.select('auth')
+        .pipe(take(1), switchMap((authState: fromAuth.State) => {
+            const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
+            return next.handle(copiedReq);
+        }));
     }
 }
--- MealsApp/src/app/shared/logging.interceptor.ts
+++ MealsApp/src/app/shared/logging.interceptor.ts
@@ -1,6 +1,5 @@
 import { Observable } from 'rxjs';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
-import 'rxjs/add/operator/do';
 import { tap } from 'rxjs/operators';
 
 export class LoggingInterceptor implements HttpInterceptor {
