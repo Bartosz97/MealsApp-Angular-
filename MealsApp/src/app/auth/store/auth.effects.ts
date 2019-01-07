c2b0f9a4c003f25efc92fafae8629bfb3a771b8d
Bartosz97 <bartoszdudek5@wp.pl>
2019-01-07 23:13:45 +0100

    Added Auth Signup and Signin

--- MealsApp/src/app/auth/store/auth.effects.ts
+++ MealsApp/src/app/auth/store/auth.effects.ts
@@ -1,5 +1,62 @@
-import { Effect } from '@ngrx/effects';
+import { Injectable } from '@angular/core';
+import { Effect, Actions, ofType } from '@ngrx/effects';
+
+import * as AuthActions from './auth.actions';
+import { map, switchMap, mergeMap } from 'rxjs/operators';
+import { fromPromise } from 'rxjs/observable/fromPromise';
+import * as firebase from 'firebase';
+
+@Injectable()
 export class AuthEffects {
-  //  @Effect()
+    @Effect()
+    authSignup = this.actions$
+    .pipe(ofType(map((action: AuthActions.TrySignup) => {
+        return action.payload;
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+        return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+    })
+    .switchMap(() => {
+        return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+        return [
+            {
+                type: AuthActions.SINGUP
+            },
+            {
+                type: AuthActions.SET_TOKEN,
+                payload: token
+            }
+        ];
+    })));
+
+    @Effect()
+    authSignin = this.actions$
+    .pipe(ofType(AuthActions.TRY_SIGNIN))
+    .map((action: AuthActions.TrySignup) => {
+        return action.payload;
+    })
+    .switchMap((authData: {username: string, password: string}) => {
+        return fromPromise(firebase.auth().signInAndRetrieveDataWithEmailAndPassword(authData.username, authData.password));
+    })
+    .switchMap(() => {
+        return fromPromise(firebase.auth().currentUser.getIdToken());
+    })
+    .mergeMap((token: string) => {
+        return [
+            {
+                type: AuthActions.SINGIN
+            },
+            {
+                type: AuthActions.SET_TOKEN,
+                payload: token
+            }
+        ];
+    });
+
+    constructor(private actions$: Actions) {
+
+    }
 
 }
