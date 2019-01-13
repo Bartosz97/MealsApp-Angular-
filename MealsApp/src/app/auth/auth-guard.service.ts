6989217f6e8e582a82dd1807bac0d38d94797b05
Bartosz97 <bartoszdudek5@wp.pl>
2019-01-13 22:27:35 +0100

    Redirecting Upon Logout

--- MealsApp/src/app/auth/auth-guard.service.ts
+++ MealsApp/src/app/auth/auth-guard.service.ts
@@ -1,4 +1,4 @@
-import { map } from 'rxjs/operators';
+import { map, take } from 'rxjs/operators';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngrx/store';
@@ -12,7 +12,8 @@ export class AuthGuard implements CanActivate {
     }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return this.store.select('auth').pipe(map((authState: fromAuth.State) => {
+        return this.store.select('auth')
+        .pipe(take(1), map((authState: fromAuth.State) => {
             return authState.authenticated;
         }));
     }
